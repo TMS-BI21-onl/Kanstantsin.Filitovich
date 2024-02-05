62117b7593213432775c64ca7401813ca4c63cc0
Kanstantsin.Filitovich <eflly0510@gmail.com>
2024-02-05 20:49:01 +0300

    added task4 test 10

---
+++ Lesson10/Test10  task4.sql
@@ -0,0 +1,10 @@
+--4)Library
+
+1. Books (book_id, title, author_id, category_id, genre_id, publisher_id, price, available)
+2. Authors (author_id, author_name, nationality, birth_date, death_date)
+3. Publishers (publisher_id, publisher_name, publication_date, location, contact_number, email)
+4. Members (member_id, member_name, address, contact_number, email)
+5. Loans (loan_id, book_id, member_id, loan_date, return_date)
+6. Categories (book_id, category_id, category_name, genge, genge_id)
+7. Rewiews (rewiew_id, book_id, member_id, rating, comment)
+
